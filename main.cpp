02f1721bc3129e6f2949f56c94e9ac931678febb
Hjg <Hjg@hjg.com>
2023-03-29 14:31:49 +0800

    提交

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+void simon(int n);
+using namespace std;
+
+int main(void)
+{
+	// using namespace std;
+	simon(3);
+	cout << "Pick an intege:"; //烽火戏诸侯
+	int n;
+	cin >> n;
+	simon(n);
+	cout << "Done";
+	return 0;
+}
+void simon(int n)
+{
+
+	cout << "simon says touch your toes" << n << " times." << endl;
+}
